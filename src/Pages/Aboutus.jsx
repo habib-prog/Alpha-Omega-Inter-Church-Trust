3f6e67f90967ba10405d1c169adb3db504956b33
unknown <kaushik.rajkamal@gmail.com>
2026-03-17 00:06:59 +0600

    Core Value

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Aboutushero from "./Aboutushero";
 import OurMission from "./OurMission";
+import CoreValue from "./CoreValue";
 
 const Aboutus = () => {
   return (
     <>
       <Aboutushero />
       <OurMission />
+      <CoreValue />
     </>
   );
 };
